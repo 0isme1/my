f341d3eb9ac7d03c079eece111006f725ecf792e
Mrtao <2416107079@qq.com>
2023-12-23 02:09:13 +0800

    aaa

--- test.c
+++
@@ -1,6 +0,0 @@
-#include<stdio.h>
-int main (void){
-
-  printf("This is a piece of code for testing!");
-  return 0;
-}
--- void1.c
+++
@@ -1,6 +0,0 @@
-#include<stdio.h>
-int mian (void){
-	printf("This is a piece of code for testing!");
-	
-	return 0;
-} 
